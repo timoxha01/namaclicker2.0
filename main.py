bfb1966a26e2e279e444258c2ee103c6250ced60
timoxha01 <timoxha001@gmail.com>
2026-01-25 18:58:05 +0500

    Updated Logic, optimization, loading screen and much more!

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ pygame.init()
 
 W, H = 1000, 800
 FPS = 60
-mode = "game"
+mode = "menu"
 
 GAME_FONT = "assets/fonts/Tiny5-Regular.ttf"
 GREY = (128, 128, 128)
@@ -22,6 +22,7 @@ font_25 = pygame.font.Font(GAME_FONT, 25)
 screen = pygame.display.set_mode((W, H))
 clock = pygame.time.Clock()
 
+inc = pygame.image.load("assets/images/UI/loading_screen.png")
 
 class Namas:
     def __init__(self, name, path, chance):
@@ -80,25 +81,26 @@ class Timer:
 
 
 class Button:
-    def __init__(self, x, y):
+    def __init__(self, x, y, cooldown=10):
         self.x = x
         self.y = y
         self.image = pygame.image.load("assets/images/UI/button.png").convert_alpha()
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
-
+        
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
 
 tamas = [
-    Namas("classic", "assets/images/tamas/classic.png", 0.9),
-    Namas("like", "assets/images/tamas/like.png", 0.6),
-    Namas("bob", "assets/images/tamas/bob.png", 0.5),
-    Namas("builder", "assets/images/tamas/builder.png", 0.4),
-    Namas("gun", "assets/images/tamas/gun.png", 0.3),
-    Namas("kinger", "assets/images/tamas/kinger.png", 0.2),
-    Namas("vibe", "assets/images/tamas/vibe.png", 0.1),
-    Namas("evil", "assets/images/tamas/evil.png", 0.01),
-    Namas("demon", "assets/images/tamas/demon.png", 0.01),
+    Namas("classic", "assets/images/tamas/classic.png", 0.99),
+    Namas("like", "assets/images/tamas/like.png", 0.69),
+    Namas("bob", "assets/images/tamas/bob.png", 0.59),
+    Namas("builder", "assets/images/tamas/builder.png", 0.49),
+    Namas("gun", "assets/images/tamas/gun.png", 0.39),
+    Namas("kinger", "assets/images/tamas/kinger.png", 0.29),
+    Namas("vibe", "assets/images/tamas/vibe.png", 0.01),
+    Namas("evil", "assets/images/tamas/evil.png", 0.001),
+    Namas("demon", "assets/images/tamas/demon.png", 0.001),
 ]
-
 
 def choose_tama(tamas):
     total_chance = sum(t.chance for t in tamas)
@@ -110,15 +112,19 @@ def choose_tama(tamas):
         if roll <= current:
             return tama
 
-
 button_to_menu_from_game = Button(20, 720)
 
-clicking_text_timer = Timer(250)
+button_to_game_from_menu = Button((W // 2) - (183 // 2), (H // 2) - (58 // 2))
 
+clicking_text_timer = Timer(250)
+loading_timer = Timer(1000)
+
+isLoading = False
 seen_tamas = set()
 tama_on_screen = tamas[0]
 total_clicks = 0
 show_boost = False
+next_mode = ""
 
 running = True
 while running:
@@ -127,15 +133,19 @@ while running:
             running = False
 
             # MouseButton действия:
-        if (
-            event.type == pygame.MOUSEBUTTONDOWN
-            and mode == "game"
-            and tama_on_screen is not None
-            and event.button == 1
-        ):
-            if button_to_menu_from_game.rect.collidepoint(event.pos):
-                mode = "menu"
-            if tama_on_screen.rect.collidepoint(event.pos):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if (
+                event.type == pygame.MOUSEBUTTONDOWN
+                and event.button == 1
+                and button_to_game_from_menu.rect.collidepoint(event.pos)
+                and mode == "menu"
+            ):
+                isLoading = True
+                next_mode = "game"
+            if button_to_menu_from_game.rect.collidepoint(event.pos) and mode == "game":
+                isLoading = True
+                next_mode = "menu"
+            if tama_on_screen.rect.collidepoint(event.pos) and mode == "game":
                 tama_on_screen.add_clicks(1, tama_on_screen.boost)
                 total_clicks += 1 * tama_on_screen.boost
                 show_boost = True
@@ -144,9 +154,8 @@ while running:
                     random.randint(0, H - 50),
                 )
                 clicking_text_timer.reset()
-                if total_clicks % 10 == 0:
-                    tama_on_screen = choose_tama(tamas)
-                    tama_on_screen.pulse()
+                tama_on_screen = choose_tama(tamas)
+                tama_on_screen.pulse()
                 # Достижение: Собрать все виды tamas
                 seen_tamas.add(tama_on_screen.name)
                 if len(seen_tamas) == len(tamas):
@@ -154,29 +163,43 @@ while running:
 
     if mode == "game" and tama_on_screen is not None:
         screen.fill(GREY)
+        button_to_menu_from_game.draw(screen)
+        screen.blit(
+            font_30.render("Меню", True, BLACK),
+            (button_to_menu_from_game.x + 52.5, button_to_menu_from_game.y + 10.5),
+        )
         tama_on_screen.update()
         tama_on_screen.draw(screen)
-        screen.blit(button_to_menu_from_game.image, button_to_menu_from_game.rect)
-        screen.blit(font_30.render("Меню", True, BLACK), (button_to_menu_from_game.x + 52.5, button_to_menu_from_game.y + 10.5))
-
         clicks_text = font_40.render(str(total_clicks), True, WHITE)
         screen.blit(clicks_text, (W // 2 - clicks_text.get_width() // 2, 440))
-        if show_boost:
+        if show_boost and mode == "game":
             screen.blit(
                 font_30.render(f"+{tama_on_screen.boost}", True, WHITE),
                 boost_pos,
             )
-            if clicking_text_timer.done():
+            if clicking_text_timer.done() and mode == "game":
                 show_boost = False
-        if total_clicks == 0:
+        if total_clicks == 0 and mode == "game":
             screen.blit(
                 font_25.render("Namatama меняется каждые 10 кликов", True, WHITE),
                 (270, 500),
             )
     if mode == "menu":
         screen.fill(BLACK)
-        screen.blit(font_40.render("MENU", True, WHITE), (400, 290))
-
+        button_to_game_from_menu.draw(screen)
+        screen.blit(
+            font_30.render("Играть", True, BLACK),
+            (button_to_game_from_menu.x + 50, button_to_game_from_menu.y + 10.5),
+        )
+    #Загрузка
+    if isLoading:
+        screen.fill(BLACK)
+        screen.blit(inc, (0, 0))
+        if loading_timer.done():
+            mode = next_mode
+            isLoading = False
+            loading_timer.reset()
+            
     pygame.display.flip()
     clock.tick(FPS)
 

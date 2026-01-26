aa80c802cd0d7136c526e0cd25bac72b0c675986
timoxha01 <timoxha001@gmail.com>
2026-01-26 21:20:14 +0500

    hint and sound for sound update

--- main.py
+++ main.py
@@ -152,6 +152,7 @@ running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            #перед закрытием добавить звук намы "byebye!"
             running = False
 
             # MouseButton действия:
